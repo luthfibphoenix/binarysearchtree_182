d6cd58c93f434481886fc0b72d727d2bdb651b6e
luthfibphoenix <lutfibahtiar2409@gmail.com>
2023-06-29 18:59:34 +0700

    16

--- binarysearchtree_182/binarysearchtree_182.cpp
+++ binarysearchtree_182/binarysearchtree_182.cpp
@@ -60,6 +60,12 @@ public:
 		currentNode = ROOT;
 		parent = NULL;
 		while ((currentNode != NULL) && (currentNode->info != element))
-		
+		{
+			parent = currentNode; if (element < currentNode->info)
+				if (element < currentNode->info)
+					currentNode = currentNode->leftchild;
+				else
+					currentNode = currentNode->rightchild;
+		}
 	}
 };
