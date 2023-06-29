29789ef391d4429cacca3b70cf37fa03e2dbfc33
luthfibphoenix <lutfibahtiar2409@gmail.com>
2023-06-29 19:03:40 +0700

    25

--- binarysearchtree_182/binarysearchtree_182.cpp
+++ binarysearchtree_182/binarysearchtree_182.cpp
@@ -107,5 +107,11 @@ public:
 			cout << "Tree is empty" << endl;
 			return;
 		}
+		if (ptr != NULL)
+		{
+			postorder(ptr->leftchild);
+			postorder(ptr->rightchild);
+			cout << ptr->info << " ";
+		}
 	}
 };
