bd5c86d5e0f81388481574333b861c4043a432fd
luthfibphoenix <lutfibahtiar2409@gmail.com>
2023-06-29 18:59:25 +0700

    15

--- binarysearchtree_182/binarysearchtree_182.cpp
+++ binarysearchtree_182/binarysearchtree_182.cpp
@@ -56,6 +56,10 @@ public:
 
 	void search(string element, Node*& parent, Node*& currentNode)
 	{
-
+		// this function searches the currentNode of the specified Node as well as the current Node of its parent
+		currentNode = ROOT;
+		parent = NULL;
+		while ((currentNode != NULL) && (currentNode->info != element))
+		
 	}
 };
