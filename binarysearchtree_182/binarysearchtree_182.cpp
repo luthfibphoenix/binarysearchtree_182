13ffacfe99037c604f0157e3d7c43cf004a95264
luthfibphoenix <lutfibahtiar2409@gmail.com>
2023-06-29 18:51:47 +0700

    menyelesaikan class node

--- binarysearchtree_182/binarysearchtree_182.cpp
+++ binarysearchtree_182/binarysearchtree_182.cpp
@@ -5,5 +5,15 @@ using namespace std;
 class node
 {
 public:
+	string info;
+	Node* leftchild;
+	Node* rightchild;
 
+	//constructor for the node class
+	Node(string i, Node * 1, Node* r)
+	{
+		info = i;
+		leftchild = 1;
+		rightchild = r;
+	}
 };
